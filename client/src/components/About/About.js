a26dab8b9ed9a95bf255e6ee2dda590122539829
Borislav Stamboliyski <b.stamboliyski@gmail.com>
2023-03-26 13:46:16 +0300

    fixed read more button

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 import { Header } from "../Header/Header";
+import { useState } from "react";
 
 
 
 export const About = () => {
 
-    
+    const [readMore, setRead] = useState(false);
+
+    const onClick = () => {
+        setRead(state => setRead(!readMore))
+    }
 
     return (
-        
+
         <section className="about_section">
-            <Header/>
+            <Header />
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-6 px-0">
@@ -29,17 +34,21 @@ export const About = () => {
                             <p>
                                 Welcome to our auction house! We specialize in selling a wide range of goods and services through the public auction process. Our team of experts evaluates and authenticates items to ensure that they are genuine and of high value.
                             </p>
-                            <p>
-                                We conduct auctions both in-person and online, allowing bidders from all over the world to participate in the bidding process. Our experienced auctioneers are here to assist you and ensure a fair and transparent auction process.
-                            </p>
-                            <p>
-                                We charge a commission on the final sale price of items, which is typically shared between the auction house and the seller. We believe that auctions are an exciting and efficient way to sell and buy high-value items, and we have a long history of playing an important role in the art market and the sale of other valuable goods.
-                            </p>
-                            <p>
-                                Thank you for choosing our auction house, and we look forward to assisting you in your buying and selling needs!
-                            </p>
-                            <Link to="/">
-                                Read More
+                            {readMore && (
+                                <>
+                                    <p>
+                                        We conduct auctions both in-person and online, allowing bidders from all over the world to participate in the bidding process. Our experienced auctioneers are here to assist you and ensure a fair and transparent auction process.
+                                    </p>
+                                    <p>
+                                        We charge a commission on the final sale price of items, which is typically shared between the auction house and the seller. We believe that auctions are an exciting and efficient way to sell and buy high-value items, and we have a long history of playing an important role in the art market and the sale of other valuable goods.
+                                    </p>
+                                    <p>
+                                        Thank you for choosing our auction house, and we look forward to assisting you in your buying and selling needs!
+                                    </p>
+                                </>)}
+
+                            <Link to="/about" onClick={onClick}>
+                                {!readMore? 'Read More' : 'Read Less'}
                             </Link>
                         </div>
                     </div>
